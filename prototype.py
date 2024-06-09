05b0610808b0cd38ab2bee3af278cd740f536118
Valacor <valyrious.informatics@gmail.com>
2024-06-09 22:30:26 +0200

    Got including of embeddings working

--- prototype.py
+++ prototype.py
@@ -1,20 +1,20 @@
 import ollama
 import chromadb
 
-class LocalOllamaEmbedding(chromadb.EmbeddingFunction[chromadb.Documents]):
+class LocalOllamaEmbedding(chromadb.EmbeddingFunction):
     def __init__(self) -> None:
         pass
 
     def __call__(self, input: chromadb.Documents) -> chromadb.Embeddings:
         embeddings = [ollama.embeddings("llama3", prompt=doc)["embedding"] for doc in input]
-        print(embeddings)
+        print(len(embeddings))
         return embeddings
 
 client = chromadb.HttpClient(host="192.168.178.47")
+col = client.get_or_create_collection(name="nice", embedding_function=LocalOllamaEmbedding())
 
-col = client.get_or_create_collection(name="testing", embedding_function=LocalOllamaEmbedding)
+col.add(ids=["id1", "id2"], documents=["Hello my friend, we are going to die!", "We are software engineers working on the goal to get big!"])
 
-col.add(ids=["id1"], documents=["Hello my friend, we are going to die!", "We are software engineers working on the goal to get big!"])
-
-result = col.get(ids=["id1"], include="embedding")
+result = col.get(ids=["id1", "id2"], include=["embeddings"])
 print(result)
+col.delete(ids=["id1", "id2"])
