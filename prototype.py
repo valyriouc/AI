d2c71a823af21da233c159231b2771fe3171241d
Valacor <valyrious.informatics@gmail.com>
2024-06-10 17:33:50 +0200

    Got a working rag application

--- prototype.py
+++ prototype.py
@@ -10,22 +10,23 @@ class LocalOllamaEmbedding(chromadb.EmbeddingFunction):
         print(len(embeddings))
         return embeddings
 
-client = chromadb.HttpClient(host="192.168.178.47")
+client = chromadb.PersistentClient(path="./testing")
 col = client.get_or_create_collection(name="nice", embedding_function=LocalOllamaEmbedding())
 
 col.add(ids=["id1", "id2"], documents=["Hello my friend, we are going to die!", "We are software engineers working on the goal to get big!"])
 
 result = col.get(ids=["id1", "id2"], include=["embeddings"])
-print(result)
-
-print(client.list_collections())
 
 olla = ollama.Client("localhost")
 
 query = input("Type in your question? ")
 query_embeddings = ollama.embeddings("llama3", prompt=query)["embedding"]
 
-result = col.query(query_texts=query, include=["embeddings"])
+result = col.query(query_texts=query)
+
+print(result)
+
+result = ollama.generate(model="llama3", prompt=f'Please generate a response for the question {query} based on the following context {result["documents"]}')
 
 print(result)
 col.delete(ids=["id1", "id2"])
