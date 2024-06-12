30e6da223b0f4c7a7da1b451266141439fdfcdfd
Valacor <valyrious.informatics@gmail.com>
2024-06-12 22:17:57 +0200

    Need of a new embedding model

--- prototype.py
+++ prototype.py
@@ -28,19 +28,25 @@ def update_database(client, documents):
     ids = [f"id{i}" for i in range(0, len(documents))]
     col.add(ids=ids, documents=[i.page_content for i in documents])
 
+### TODO: We need a better embedding model 
 
 def main():
     client = chromadb.PersistentClient(path="./testing")
     olla = ollama.Client("localhost")
 
     query = input("Type in your question? ")
-    query_embeddings = ollama.embeddings("llama3", prompt=query)["embedding"]
+    embeddings = ollama.embeddings(model="llama3", prompt=query, options={"num_predict": 384, "temperature": 384})
+    print(embeddings)
 
     col = client.get_collection(name="books")
-    result = col.query(query_texts=query)
+    result = col.query(query_embeddings=embeddings["embedding"])
 
-    result = ollama.generate(model="llama3", prompt=f'Please generate a response for the question {query} based on the following context {result["documents"]}')
+    prompt = f"""
+    Please generate a repsonse for the questions {query} based on the given context {result["documents"]}"
+    """
 
+    result = ollama.generate(model="llama3", prompt=prompt)
+    
     print(result)
 
 if __name__ == "__main__":
