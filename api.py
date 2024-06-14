810bfd9c6c360c17618e0b1094ba93d2167b92de
Valacor <valyrious.informatics@gmail.com>
2024-06-14 22:31:31 +0200

    Wrap prototype into a web api

--- api.py
+++ api.py
@@ -1,19 +1,94 @@
+import chromadb
 import flask
+import ollama
+import uuid
+import os 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from flask import request
+app = flask.Flask(__name__)
 
-app = flask.Flask(__name__) 
+# This class is a custom embedding function for local Ollama embeddings.
+class LocalOllamaEmbedding(chromadb.EmbeddingFunction):
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, input: chromadb.Documents) -> chromadb.Embeddings:
+        embeddings = [ollama.embeddings("nomic-embed-text", prompt=doc)["embedding"] for doc in input]
+        return embeddings
+    
+def update_database(client, topic: str, documents) -> chromadb.Collection:
+    col = client.get_or_create_collection(name=topic, embedding_function=LocalOllamaEmbedding())
+    count = col.count()
+    ids = [f"id{i}" for i in range(count, count + len(documents))]
+    col.add(ids=ids, documents=[i.page_content for i in documents])
+    return col
+
+def load_document(path: str): 
+    loader = PyPDFLoader(path)
+    documents = loader.load()
+    split = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    documents = split.split_documents(documents=documents)
+    return documents
+
+def handle_question(client: chromadb.PersistentClient, context: dict):
+    question_emb = ollama.embeddings("nomic-embed-text", prompt=context["question"])["embedding"]
+    col = client.get_collection(name=context["topic"], embedding_function=LocalOllamaEmbedding())
+    result = col.query(query_embeddings=question_emb)
+    prompt = f"""
+    Please generate a repsonse for the questions {context['question']} based on the given context {result["documents"]}"
+    """
+    return ollama.generate(model="llama3", prompt=prompt)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET": 
-        return "Hello"
-    else:
-        json = request.get_json()
+    if flask.request.method == "GET":
+        pass
+    elif flask.request.method == "POST":
+        client = chromadb.PersistentClient()
+        return handle_question(client, flask.request.get_json()) 
 
-@app.route("/embed", methods=["GET", "POST"]) 
+@app.route("/embed/", methods=["POST"]) 
 def embed():
-    pass
+    topic = flask.request.args.get("topic")
+    raw_bytes = flask.request.get_data()
+    filename = None
+    while True:
+        filename = str(uuid.uuid4())
+        if (not os.path.exists(os.path.join(".", "tmp", f"{filename}.pdf"))):
+            break
+    filepath = os.path.join(".", "tmp", f"{filename}.pdf")
+    with open(filepath, "wb") as fobj:
+        fobj.write(raw_bytes)
+    documents = load_document(filepath)
+    client = chromadb.PersistentClient()
+    update_database(client, topic, documents)
+    os.remove(filepath)
+    return {"status": "200", "msg": "Embedded document!"}
 
-if __name__ == "__main__":  
-    app.run("0.0.0.0", "9222")
+@app.route("/list/collections/", methods=["GET"])
+def list_collections():
+    client = chromadb.PersistentClient()
+    return client.list_collections()
+
+def parse_args(args):
+    parsed = {
+        "address": "127.0.0.1",
+        "port": 7120,
+    }
+    for i in range(0, len(args)):
+        if args[i] == "-h":
+            parsed["address"] = args[i+1]
+            i += 1
+        if args[i] == "-p":
+            parsed["port"] = int(args[i+1])
+            i += 1
+    return parsed
+
+def main(args):
+    parsed = parse_args(args)
+    app.run(host=parsed["address"], port=parsed["port"])
+
+if __name__ == "__main__":
+    import sys 
+    main(sys.argv)
