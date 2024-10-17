b949658f4924b9561228eed8ee7ee8fc13b81eea
Valacor <valyrious.informatics@gmail.com>
2024-10-17 18:51:23 +0200

    Class library for AI connections

--- GeminiTesting/Program.cs
+++ GeminiTesting/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal static class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
