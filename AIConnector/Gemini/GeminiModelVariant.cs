aa10e4a6dc57c589af902407701fff18850c4539
Valacor <valyrious.informatics@gmail.com>
2024-10-20 22:42:12 +0200

    String representation of gemini modes

--- AIConnector/Gemini/GeminiModelVariant.cs
+++ AIConnector/Gemini/GeminiModelVariant.cs
@@ -32,6 +32,23 @@ internal static class EnumExtensions
                 return "text-embedding-004";
             case GeminiModelVariant.AQA:
                 return "aqa";
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    public static string AsString(this GeminiMode self)
+    {
+        switch (self)
+        {
+            case GeminiMode.ContentGeneration:
+                return "generateContent";
+            case GeminiMode.ContentGenerationStream:
+                return "streamGenerateContent";
+            case GeminiMode.Embed:
+                return "embedContent";
+            default:
+                throw new NotSupportedException();
         }
     }
 }
