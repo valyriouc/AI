046cc2aa40623cbc0d235d7fe18498bc77e448f8
Valacor <valyrious.informatics@gmail.com>
2024-10-26 20:03:21 +0200

    Implement gemini safety settings

---
+++ AIConnector/Gemini/GeminiSafety.cs
@@ -0,0 +1,51 @@
+﻿namespace AIConnector.Gemini;
+
+public enum GeminiSafetyCategory
+{
+    Harassment,
+    HateSpeech,
+    SexuallyExplicit,
+    DangerousContent,
+    CivicIntegrity
+}
+
+internal static class GeminiSafetyCategoryExtensions
+{
+    public static string AsString(this GeminiSafetyCategory self)
+    {
+        return self switch
+        {
+            GeminiSafetyCategory.Harassment => "HARM_CATEGORY_HARASSMENT",
+            GeminiSafetyCategory.HateSpeech => "HATE_CATEGORY_HATE_SPEECH",
+            GeminiSafetyCategory.SexuallyExplicit => "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            GeminiSafetyCategory.DangerousContent => "HARM_CATEGORY_DANGEROUS_CONTENT",
+            GeminiSafetyCategory.CivicIntegrity => "HARM_CATEGORY_CIVIC_INTEGRITY",
+            _ => throw new NotImplementedException()
+        };
+    }
+}
+
+public enum GeminiSafetyFiltering
+{
+    Unclear,
+    None,
+    High, 
+    Medium,
+    Low
+}
+
+internal static class GeminiSafetyFilteringExtensions
+{
+    public static string AsString(this GeminiSafetyFiltering self)
+    {
+        return self switch
+        {
+            GeminiSafetyFiltering.Unclear => "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
+            GeminiSafetyFiltering.None => "BLOCK_NONE",
+            GeminiSafetyFiltering.High => "BLOCK_ONLY_HIGH",
+            GeminiSafetyFiltering.Medium => "BLOCK_MEDIUM_AND_ABOVE",
+            GeminiSafetyFiltering.Low => "BLOCK_LOW_AND_ABOVE",
+            _ => throw new NotImplementedException()
+        };
+    }
+}
