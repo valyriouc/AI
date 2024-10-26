dec3d263e872642e87dd43671f8fb2a58b301df2
Valacor <valyrious.informatics@gmail.com>
2024-10-26 19:05:10 +0200

    Implement gemini api error handling

---
+++ AIConnector/Common/HttpResponseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AIConnector.Gemini;
+
+namespace AIConnector.Common;
+
+internal static class HttpResponseExtensions
+{
+    public static async Task ThrowOnGeminiErrorAsync(this HttpResponseMessage self)
+    {
+        if (!self.IsSuccessStatusCode)
+        {
+            throw await GeminiApiException.FromResponseAsync(self);
+        }
+    }
+}
---
+++ AIConnector/Gemini/GeminiException.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AIConnector.Gemini;
+
+public class GeminiException(string message) : Exception(message);
+
+public sealed class GeminiApiException(uint error, string status, string errorMessage)
+    : Exception(BuildMessage(error, status, errorMessage))
+{
+    public uint Error { get; } = error;
+
+    public string Status { get; } = status;
+
+    public string ErrorMessage { get; } = errorMessage;
+
+    private static string BuildMessage(uint error, string status, string errorMessage)
+    {
+        StringBuilder sb = new();
+        sb.AppendLine($"Gemini send back error {error}");
+        sb.AppendLine($"Status: {status}");
+        sb.AppendLine($"Message: {errorMessage}");
+        return sb.ToString();
+    }
+
+    public static async Task<GeminiApiException> FromResponseAsync(HttpResponseMessage response)
+    {
+        GeminiErrorWrapper? errorWrapper = await response.Content
+            .ReadFromJsonAsync<GeminiErrorWrapper>();
+
+        if (errorWrapper is null)
+        {
+            throw new GeminiException("Could not get error message from response!");
+        }
+
+        GeminiError error = errorWrapper.Value.Error;
+
+        return new GeminiApiException(
+            error.Error,
+            error.Status,
+            error.Message);
+    }
+}
+
+[method: JsonConstructor]
+file readonly struct GeminiErrorWrapper(GeminiError error)
+{
+    [JsonPropertyName("error")]
+    public GeminiError Error { get; } = error;
+}
+
+[method: JsonConstructor]
+file readonly struct GeminiError(uint error, string status, string message)
+{
+    [JsonPropertyName("error")]
+    public uint Error { get; } = error;
+
+    [JsonPropertyName("status")]
+    public string Status { get; } = status;
+
+    [JsonPropertyName("message")]
+    public string Message { get; } = message;
+}
