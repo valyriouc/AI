a5449e5999f49a6f1a66ae4dbb72e9bffff14b14
Valacor <valyrious.informatics@gmail.com>
2024-10-27 18:43:10 +0100

    Implement gemini file api

---
+++ AIConnector/Gemini/Files/GeminiFile.cs
@@ -0,0 +1,47 @@
+﻿namespace AIConnector.Gemini.Files;
+
+public readonly struct GeminiFile
+{
+    private readonly string filepath;
+    
+    public string FileName { get; }
+
+    public ReadOnlyMemory<byte> FileBytes { get; }
+    
+    public GeminiFileMimeType MimeType { get; }
+
+    public int FileSize => FileBytes.Length;
+    
+    public GeminiFile(string filepath)
+    {
+        if (!File.Exists(filepath))
+        {
+            throw new FileNotFoundException("File not found", filepath);
+        }
+        
+        this.filepath = filepath;
+        FileName = Path.GetFileName(filepath);
+        MimeType = DetermineFileType(FileName);
+        FileBytes = File.ReadAllBytes(this.filepath);
+    }
+
+    private static GeminiFileMimeType DetermineFileType(string filename)
+    {
+        string extension = Path.GetExtension(filename);
+
+        return extension switch
+        {
+            ".md" => GeminiFileMimeType.Markdown,
+            ".pdf" => GeminiFileMimeType.Pdf,
+            ".txt" => GeminiFileMimeType.Text,
+            ".js" => GeminiFileMimeType.JavaScript,
+            ".py" => GeminiFileMimeType.Python,
+            ".html" or ".htm" or ".htmlx" => GeminiFileMimeType.Html,
+            ".css" => GeminiFileMimeType.Css,
+            ".xml" => GeminiFileMimeType.Xml,
+            ".rtf" => GeminiFileMimeType.Rtf,
+            ".csv" => GeminiFileMimeType.Csv,
+            _ => throw new GeminiException($"File type {extension} not supported!")
+        };
+    }
+}
---
+++ AIConnector/Gemini/Files/GeminiFileUploader.cs
@@ -0,0 +1,110 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using AIConnector.Common;
+
+namespace AIConnector.Gemini.Files;
+
+public sealed class GeminiFileUploader(
+    string apiKey) : IDisposable
+{
+    private const string UploadUrl = "https://generativelanguage.googleapis.com/upload/v1beta/files/";
+    private const string FileUrl = "https://generativelanguage.googleapis.com/v1beta/files/";
+
+    private readonly HttpClient httpClient = new();
+
+    public async Task UploadFileAsync(GeminiFile file, CancellationToken cancellationToken)
+    {
+        using HttpRequestMessage initRequest = CreateInitUploadRequest(file);
+        
+        using HttpResponseMessage response = await httpClient.SendAsync(initRequest, cancellationToken);
+        
+        await response.ThrowOnGeminiErrorAsync();
+
+        string uploadUrl = response.Headers.GetValues("x-goog-upload-url").First();
+
+        using HttpRequestMessage finalizeRequest = CreateFinalizeUploadRequest(uploadUrl, file);
+        
+        using HttpResponseMessage finalize = await httpClient.SendAsync(finalizeRequest, cancellationToken);
+        
+        await response.ThrowOnGeminiErrorAsync();
+    }
+
+    private HttpRequestMessage CreateInitUploadRequest(GeminiFile file)
+    {
+        StringBuilder sb = new StringBuilder(UploadUrl);
+        sb.Append($"?key={apiKey}");
+        
+        HttpRequestMessage request = new HttpRequestMessage(
+            HttpMethod.Get, 
+            sb.ToString());
+        
+        request.Headers.Add("X-Goog-Upload-Protocol", "resumable");
+        request.Headers.Add("X-Goog-Upload-Command", "start");
+        request.Headers.Add("X-Goog-Upload-Header-Content-Length", $"{file.FileSize}");
+        request.Headers.Add("X-Goog-Upload-Header-Content-Type", file.MimeType.AsString());
+        
+        string json = 
+            $$"""
+            {
+              "file": 
+              {
+                "display_name": {{file.FileName}}
+              }  
+            }
+            """;
+
+        request.Content = new StringContent(json);
+        request.Content!.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+        return request;
+    }
+
+    private HttpRequestMessage CreateFinalizeUploadRequest(string baseUrl, GeminiFile file)
+    {
+        HttpRequestMessage message = new HttpRequestMessage(
+            HttpMethod.Get, 
+            $"{baseUrl}?key={apiKey}");
+        
+        message.Headers.Add("Content-Length", file.FileSize.ToString());
+        message.Headers.Add("X-Goog-Upload-Offset", "0");
+        message.Headers.Add("X-Goog-Upload-Command", "upload, finalize");
+        
+        message.Content = new ByteArrayContent(file.FileBytes.ToArray());
+        return message;
+    }
+
+    public async Task<string> GetFileMetadataAsync(string filename, CancellationToken cancellationToken)
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync(
+            $"{FileUrl}?key={apiKey}", 
+            cancellationToken);
+
+        await response.ThrowOnGeminiErrorAsync();
+        
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    public async Task<string> ListFilesAsync(CancellationToken cancellationToken)
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync(
+            $"{FileUrl}?key={apiKey}",
+            cancellationToken);
+
+        await response.ThrowOnGeminiErrorAsync();
+        
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    public async Task DeleteFileAsync(string filename, CancellationToken cancellationToken)
+    {
+        using HttpResponseMessage response = await httpClient.DeleteAsync(
+            $"{FileUrl}{filename}?key={apiKey}");
+        
+        await response.ThrowOnGeminiErrorAsync();
+    }
+
+    public void Dispose()
+    {
+        // TODO release managed resources here
+        httpClient.Dispose();
+    }
+}
