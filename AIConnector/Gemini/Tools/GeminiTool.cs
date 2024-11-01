2c46f1cc0ed33dddede03a41a6c2705915e6cbbd
Valacor <valyrious.informatics@gmail.com>
2024-11-01 20:41:01 +0100

    The gemini tool struct

---
+++ AIConnector/Gemini/Tools/GeminiTool.cs
@@ -0,0 +1,14 @@
+﻿namespace AIConnector.Gemini.Tools;
+
+public class GeminiTool(
+    List<FunctionDeclaration> functionDeclarations,
+    GoogleSearchRetrieval googleSearchRetrieval,
+    CodeExecution codeExecution)
+{
+    public List<FunctionDeclaration> FunctionDeclarations { get; } = functionDeclarations;
+
+    public GoogleSearchRetrieval GoogleSearchRetrieval { get; } = googleSearchRetrieval;
+
+    public CodeExecution CodeExecution { get; } = codeExecution;
+
+}
