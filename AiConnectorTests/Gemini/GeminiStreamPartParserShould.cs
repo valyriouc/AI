37729628756793fa2db975836ee7613ea9b11dba
Valacor <valyrious.informatics@gmail.com>
2024-11-01 18:07:25 +0100

    Adding a unit test project

---
+++ AiConnectorTests/Gemini/GeminiStreamPartParserShould.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using AIConnector.Gemini;
+
+namespace AiConnectorTests.Gemini;
+
+public class GeminiStreamPartParserShould
+{
+    [Fact]
+    public void Test1()
+    {
+        string parts =
+            """
+            Data: {
+                "hello": 100,
+                "nice": "world"
+            }
+            
+            """;
+        
+        ReadOnlySpan<byte> buffer = Encoding.UTF8.GetBytes(parts);
+        buffer = GeminiStreamPartParser.Parse(buffer, out List<string> remains);
+        
+        Assert.Equal(0, buffer.Length);
+        Assert.Single(remains);
+
+        string expected =
+            """
+            {
+                "hello": 100,
+                "nice": "world"
+            }
+            """;
+        
+        Assert.Equal(expected, remains[0]);
+        
+    }
+}
